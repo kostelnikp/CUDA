541d034d88188740d26c49c301fcd0b762901fb5
kostelnikp <95362795+kostelnikp@users.noreply.github.com>
2023-05-04 12:50:38 +0200

    Update cuda_img.h

--- cuda_img.h
+++ cuda_img.h
@@ -12,17 +12,25 @@
 // ***********************************************************************
 
 #pragma once
-
 #include <opencv2/core/mat.hpp>
-
 // Structure definition for exchanging data between Host and Device
 struct CudaImg
 {
-  uint3 m_size;             // size of picture
-  union {
-      void   *m_p_void;     // data of picture
-      uchar1 *m_p_uchar1;   // data of picture
-      uchar3 *m_p_uchar3;   // data of picture
-      uchar4 *m_p_uchar4;   // data of picture
-  };
+    uint3 m_size;             // size of picture
+    union {
+        void* m_p_void;     // data of picture
+        uchar1* m_p_uchar1;   // data of picture
+        uchar3* m_p_uchar3;   // data of picture
+        uchar4* m_p_uchar4;   // data of picture
+    };
+    _host_
+_device_ uchar4& at4(int y, int x) 
+    { 
+        return m_p_uchar4[y * m_size.x + x];
+    }
+    _host_
+_device_ uchar3& at3(int y, int x) 
+    { 
+        return m_p_uchar3[y * m_size.x + x];
+    }
 };
